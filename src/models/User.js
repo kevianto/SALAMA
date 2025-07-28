66acf1274c4bc0fb64bec1c1f00ea1edd28de0df
kevianto <kevinshahale746@gmail.com>
2025-07-28 17:52:57 +0300

    user

--- src/models/User.js
+++ src/models/User.js
@@ -1,22 +1,16 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  location: {
+    name: { type: String, required: true },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
   },
-  phone: {
-    type: String,
-    required: true
-  },
-location: {
-  latitude: { type: Number, required: true },
-  longitude: { type: Number, required: true },
-  name: { type: String, required: true }
-}
-
-
 });
+
+
 
 const user = mongoose.model('User', UserSchema);
 export default user;
